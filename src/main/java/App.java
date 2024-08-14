6897cbf2bcbb85854007ad3e12ee4c3fb923cdaf
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2024-08-13 21:37:38 -0300

    fim

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,15 +6,17 @@ import org.example.permfunction.PermFunctionIndFactory;
 
 public class App {
     public static void main(String[] args) {
-        int nVar= 2; //dimensoes
+        int nVar = 2; //dimensoes
+        int nGenes = 8;
         int nPop = 20; // Tamanho da população
         int elite = 4; // Tamanho da elite
         int nGer = 2000; // Número de gerações
 
         AG ag = new AG();
-        PermFunctionIndFactory permFactory = new PermFunctionIndFactory(nVar);
+
+        PermFunctionIndFactory permFactory = new PermFunctionIndFactory(nGenes);
         LangermannFunctionIndFactory langermannFactory = new LangermannFunctionIndFactory(nVar);
-        DixonPriceFunctionIndFactory dixonPricedFactory = new DixonPriceFunctionIndFactory(nVar);
+        DixonPriceFunctionIndFactory dixonPricedFactory = new DixonPriceFunctionIndFactory(nGenes);
         Individuo melhorIndividuoPerm = ag.executar(permFactory, nPop, elite, nGer);
         Individuo melhorIndividuoLangermann = ag.executar(langermannFactory, nPop, elite, nGer);
         Individuo melhorIndividuoDixon = ag.executar(dixonPricedFactory, nPop, elite, nGer);
